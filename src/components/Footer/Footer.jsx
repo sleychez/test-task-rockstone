844939c63a4943857bf3db68c49de3978f383913
sleychez <v0401podtyagin@mail.ru>
2023-02-25 01:47:34 +0300

    change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="App-footer">
-            <p>Virtual ATM-Machine 2021</p>
+            <p>Virtual ATM-Machine 2023</p>
         </footer>
     )
 }
