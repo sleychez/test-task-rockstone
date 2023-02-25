3d65e10486c301bd82dd317b6307cdfc02d0e95e
sleychez <v0401podtyagin@mail.ru>
2023-02-25 14:33:46 +0300

    change

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './components/redux/redux-store';
-import { BrowserRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
